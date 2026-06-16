92ab7472c7e2297a5f99a4bb704d2ea0b1ca9701
chitransh srivastava <chitranshcsk2005@gmail.com>
2026-06-16 12:56:04 +0530

    Implement function to remove Nth node from end of list

---
+++ Week_3/Day_2/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // Dummy node to handle edge cases
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // Move fast n + 1 steps ahead
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        // Move both pointers
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // Remove the target node
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
